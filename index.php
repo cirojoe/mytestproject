4807d9c1516745cdf5f45f948c69f4e09530201c
unknown <CiceroJ@cicero_joseph.rsp.local>
2016-03-08 09:57:23 +0200

    Change text in index page

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>My first Page</title>
 </head>
 
 <body>
 
-This is the PHP file
+My Great index.php file
 
 </body>
 </html>
